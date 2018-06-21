cbf8812fccd6609f2c9b4e5477e02cb72ee890bc
srinivas <srinidas@cisco.com>
2018-06-21 12:49:47 +0530

    crud

---
+++ react-contact-manager/src/contacts-data.js
@@ -0,0 +1,38 @@
+export const contacts = [
+    {
+      _id: "1",
+      name: {
+        first:"John",
+        last:"Doe"
+      },
+      phone:"555",
+      email:"john@gmail.com"
+    },
+    {
+      _id: "2",
+      name: {
+        first:"Bruce",
+        last:"Wayne"
+      },
+      phone:"777",
+      email:"bruce.wayne@gmail.com"
+    },
+    {
+      _id: "3",
+      name: {
+        first:"SRI",
+        last:"Dasari"
+      },
+      phone:"555",
+      email:"XXX@gmail.com"
+    },
+    {
+      _id: "4",
+      name: {
+        first:"vasu",
+        last:"D"
+      },
+      phone:"777",
+      email:"YYYYY@gmail.com"
+    }
+  ];
